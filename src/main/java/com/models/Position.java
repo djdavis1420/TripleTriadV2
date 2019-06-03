92d739dee61c15c72eaa84baa45b00d23e7684fb
Dylan Davis <djdavis1420@gmail.com>
2019-06-02 19:42:32 -0500

    Added Model for Position

---
+++ src/main/java/com/models/Position.java
@@ -0,0 +1,34 @@
+package com.models;
+
+public class Position {
+
+    private String name;
+    private String owner;
+    private Card card;
+
+    public Position() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+}
