d0bc12e87b4949bc03431e313265707a345d961d
Dylan Davis <djdavis1420@gmail.com>
2019-06-04 13:52:26 -0500

    Created Application Class with Spring Annotation

---
+++ src/main/java/com/TripleTriad.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleTriad {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleTriad.class, args);
+    }
+
+}
