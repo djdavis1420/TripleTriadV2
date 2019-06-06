0574cf5168ecd7c3ae843dbb29e3173891d28c2c
Dylan Davis <djdavis1420@gmail.com>
2019-06-06 16:46:22 -0500

    Deck Service Can Generate Player Deck & Starter Deck

--- src/main/java/com/services/DeckService.java
+++ src/main/java/com/services/DeckService.java
@@ -1,5 +1,6 @@
 package com.services;
 
+import com.database.CardsDatabase;
 import com.database.DecksDatabase;
 import com.models.Card;
 import com.models.Player;
@@ -8,14 +9,25 @@ import java.util.List;
 
 public class DeckService {
 
-    private DecksDatabase database;
+    private DecksDatabase decksDatabase;
+    private CardsDatabase cardsDatabase;
+    private List<Card> deckCache;
 
-    public DeckService(DecksDatabase database) {
-        this.database = database;
+    public DeckService(DecksDatabase decksDatabase, CardsDatabase cardsDatabase) {
+        this.decksDatabase = decksDatabase;
+        this.cardsDatabase = cardsDatabase;
     }
 
     public Boolean doesDeckExist(Player player) {
-        List<Card> deck = database.getDeckByUsername(player);
+        List<Card> deck = decksDatabase.getDeckByUsername(player);
+        deckCache = deck;
         return deck.stream().findAny().isPresent();
+    }
+
+    public List<Card> generateDeck(Player player) {
+        if (!doesDeckExist(player)) {
+            return cardsDatabase.getStarterDeck();
+        }
+        return deckCache;
     }
 }
--- src/test/java/com/services/DeckServiceTest.java
+++ src/test/java/com/services/DeckServiceTest.java
@@ -1,5 +1,6 @@
 package com.services;
 
+import com.database.CardsDatabase;
 import com.database.DecksDatabase;
 import com.models.Card;
 import com.models.Player;
@@ -10,25 +11,26 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class DeckServiceTest {
 
     private DeckService deckService;
-    private DecksDatabase database;
+    private DecksDatabase decksDatabase;
+    private CardsDatabase cardsDatabase;
 
     @Before
     public void setup() {
-        database = mock(DecksDatabase.class);
-        deckService = new DeckService(database);
+        decksDatabase = mock(DecksDatabase.class);
+        cardsDatabase = mock(CardsDatabase.class);
+        deckService = new DeckService(decksDatabase, cardsDatabase);
     }
 
     @Test
     public void doesDeckExist_shouldReturnFalseWhenPlayerDeckDoesNotExist() {
         Player player = new Player();
         List<Card> deck = Collections.emptyList();
-        when(database.getDeckByUsername(player)).thenReturn(deck);
+        when(decksDatabase.getDeckByUsername(player)).thenReturn(deck);
 
         Boolean actual = deckService.doesDeckExist(player);
 
@@ -39,10 +41,33 @@ public class DeckServiceTest {
     public void doesDeckExist_shouldReturnTrueWhenPlayerDeckDoesExist() {
         Player player = new Player();
         List<Card> deck = Collections.singletonList(new Card());
-        when(database.getDeckByUsername(player)).thenReturn(deck);
+        when(decksDatabase.getDeckByUsername(player)).thenReturn(deck);
 
         Boolean actual = deckService.doesDeckExist(player);
 
         assertEquals(true, actual);
+    }
+
+    @Test
+    public void generateDeck_shouldGenerateStarterDeckWhenDeckDoesNotExist() {
+        Player player = new Player();
+        List<Card> deck = Collections.singletonList(new Card());
+        when(decksDatabase.getDeckByUsername(player)).thenReturn(Collections.emptyList());
+        when(cardsDatabase.getStarterDeck()).thenReturn(deck);
+
+        List<Card> actual = deckService.generateDeck(player);
+
+        assertEquals(deck, actual);
+    }
+
+    @Test
+    public void generateDeck_shouldReturnCachedDeck() {
+        Player player = new Player();
+        List<Card> deck = Collections.singletonList(new Card());
+        when(decksDatabase.getDeckByUsername(player)).thenReturn(deck);
+
+        List<Card> actual = deckService.generateDeck(player);
+
+        assertEquals(deck, actual);
     }
 }
